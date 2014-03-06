c28ce83083b3222b3425d0e21090a19e3500f39f
Matteo Giaccone <matteo.giaccone@welaika.com>
2014-03-06 14:25:29 +0000

    Ruby compatibility issue, v. 0.0.2

--- lib/weather.rb
+++ lib/weather.rb
@@ -10,7 +10,9 @@ class Weather
     "Now in #{location} the weather is: " + openweather_situation(location)['weather'][0]['description']
   end
 
-  private def openweather_situation(location)
+  private
+
+  def openweather_situation(location)
     @situation ||= JSON.load(open("http://api.openweathermap.org/data/2.5/weather?q=#{location}"))
   end
 end
--- weather.gemspec
+++ weather.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name        = 'weather-mg'
-  s.version     = '0.0.1'
+  s.version     = '0.0.2'
   s.date        = '2014-03-06'
   s.summary     = "Weather"
   s.description = "Weather command line application"
